8d4a9ef8fdcd751e3fbfdc51496f6db704b17c35
Mateo Etchepare <mateoetchepare@gmail.com>
2023-01-19 13:08:10 -0300

    Initial Commit

---
+++ mySafePocket/mySafePocketApp.swift
@@ -0,0 +1,17 @@
+//
+//  mySafePocketApp.swift
+//  mySafePocket
+//
+//  Created by Mateo Etchepare on 19/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct mySafePocketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
